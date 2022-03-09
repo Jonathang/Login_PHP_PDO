dbee029af978bc722a94a38be96e7f22af62ed2a
Jonathang <jgarcia2888@gmail.com>
2022-03-09 10:25:46 -0600

    validaciones de cantidad

--- controlador/codigoRegistroEstudiante.php
+++ controlador/codigoRegistroEstudiante.php
@@ -3,8 +3,6 @@
     
     if (isset($_POST['id_nombre']) && isset($_POST['matricula']) && isset($_POST['programacion'])  && isset($_POST['matematicas'])  && isset($_POST['algoritmos'])  && isset($_POST['logica'])  && isset($_POST['so'])  && isset($_POST['bd'])) {
 
-        
-
         $sql = "INSERT INTO calificaciones (matricula, programacion, matematicas, algoritmos, logica, so, bd, usuario_id_fk) VALUES (:matricula, :programacion, :matematicas, :algoritmos, :logica, :so, :bd, :id_nombre)";
         $stmt = $conn->prepare($sql);  
 
@@ -30,7 +28,7 @@
         }
         // NOMBRE ESTUDIANTE - MATRICULA
 
-        if(empty($_POST['matricula']) || strlen($matricula) == 8 || !preg_match('`[0-9]`', $matricula)){
+        if(empty($_POST['matricula']) || strlen($matricula) > 8 || !preg_match('`[0-9]`', $matricula)){
              echo"
              <script language='javascript'>
              alert('El campo matricula no puede estar vacio y debe contener solo números')
@@ -41,7 +39,7 @@
         }
         // MATRICULA
 
-         if(empty($_POST['programacion']) || strlen($programacion) > 4 || !preg_match('`[0-9]`',$programacion)){
+         if(empty($_POST['programacion']) || strlen($programacion) > 3 || !preg_match('`[0-9]`',$programacion)){
              echo"
              <script language='javascript'>
              alert('Digite una calificación no mayor a 3 digitos')
@@ -52,7 +50,7 @@
         }
         // PROGRAMACION
 
-        if(empty($_POST['matematicas']) || strlen($matematicas) > 4 || !preg_match('`[0-9]`',$matematicas)){
+        if(empty($_POST['matematicas']) || strlen($matematicas) > 3 || !preg_match('`[0-9]`',$matematicas)){
              echo"
              <script language='javascript'>
              alert('Digite una calificación no mayor a 3 digitos')
@@ -63,7 +61,7 @@
         }
         // MATEMATICAS
 
-        if(empty($_POST['algoritmos']) || strlen($algoritmos) > 4 || !preg_match('`[0-9]`',$algoritmos)){
+        if(empty($_POST['algoritmos']) || strlen($algoritmos) > 3 || !preg_match('`[0-9]`',$algoritmos)){
              echo"
              <script language='javascript'>
              alert('Digite una calificación no mayor a 3 digitos')
@@ -75,7 +73,7 @@
         // ALGORITMOS
 
 
-        if(empty($_POST['logica']) || strlen($logica) > 4 || !preg_match('`[0-9]`',$logica)){
+        if(empty($_POST['logica']) || strlen($logica) > 3 || !preg_match('`[0-9]`',$logica)){
              echo"
              <script language='javascript'>
              alert('Digite una calificación no mayor a 3 digitos')
@@ -86,7 +84,7 @@
         }
         // LOGICA
 
-        if(empty($_POST['so']) || strlen($so) > 4 || !preg_match('`[0-9]`',$so)){
+        if(empty($_POST['so']) || strlen($so) > 3 || !preg_match('`[0-9]`',$so)){
              echo"
              <script language='javascript'>
              alert('Digite una calificación no mayor a 3 digitos')
@@ -97,7 +95,7 @@
         }
         // SO
 
-        if(empty($_POST['bd']) || strlen($bd) > 4 || !preg_match('`[0-9]`',$bd)){
+        if(empty($_POST['bd']) || strlen($bd) > 3 || !preg_match('`[0-9]`',$bd)){
              echo"
              <script language='javascript'>
              alert('Digite una calificación no mayor a 3 digitos')
