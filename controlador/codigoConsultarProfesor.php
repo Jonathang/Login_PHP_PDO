3f28633ef1648b6711376e30efd445e29e3f6c13
Jonathang <jgarcia2888@gmail.com>
2022-03-14 19:43:10 -0600

    Update codigoConsultarProfesor.php

--- controlador/codigoConsultarProfesor.php
+++ controlador/codigoConsultarProfesor.php
@@ -3,7 +3,9 @@
     session_start();
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = $conn->prepare('SELECT calificaciones.matricula, calificaciones.programacion, calificaciones.matematicas, calificaciones.algoritmos, calificaciones.logica, calificaciones.so, calificaciones.bd, usuarios.Nombre_usuario FROM calificaciones INNER JOIN usuarios ON calificaciones.usuario_id_fk = usuarios.ID_usuario');
+    $sql = $conn->prepare('SELECT calificaciones.matricula, calificaciones.programacion, 
+    calificaciones.matematicas, calificaciones.algoritmos, calificaciones.logica, calificaciones.so, 
+    calificaciones.bd, usuarios.Nombre_usuario FROM calificaciones INNER JOIN usuarios ON calificaciones.usuario_id_fk = usuarios.ID_usuario');
     $sql->execute();
     $stm = $sql->fetchAll(PDO::FETCH_OBJ); 
 
