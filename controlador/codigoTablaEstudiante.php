97985dc9a86d62a6339ba3edde59b8bf99213398
Jonathang <jgarcia2888@gmail.com>
2022-03-14 19:44:36 -0600

    Update codigoTablaEstudiante.php

--- controlador/codigoTablaEstudiante.php
+++ controlador/codigoTablaEstudiante.php
@@ -3,7 +3,9 @@
 
    if (isset($_SESSION['usuario_id'])) {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = $conn->prepare('SELECT calificaciones.programacion, calificaciones.matematicas, calificaciones.algoritmos, calificaciones.logica, calificaciones.so, calificaciones.bd FROM calificaciones INNER JOIN usuarios ON usuarios.ID_usuario= calificaciones.usuario_id_fk WHERE usuarios.ID_usuario= :id');
+    $sql = $conn->prepare('SELECT calificaciones.programacion, calificaciones.matematicas, 
+    calificaciones.algoritmos, calificaciones.logica, calificaciones.so, calificaciones.bd 
+    FROM calificaciones INNER JOIN usuarios ON usuarios.ID_usuario= calificaciones.usuario_id_fk WHERE usuarios.ID_usuario= :id');
     $sql->bindParam(':id', $_SESSION['usuario_id']);
     $sql->execute();
     $stm = $sql->fetchAll(PDO::FETCH_OBJ); 
